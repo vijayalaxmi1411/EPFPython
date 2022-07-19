e52ec054080981aa3831ad1c4848d45039be09c9
vijayalaxmi1411 <92300605+vijayalaxmi1411@users.noreply.github.com>
2022-07-19 11:21:47 +0530

    Update EPFScript.bat

--- EPFScript.bat
+++ EPFScript.bat
@@ -1,1 +1,1 @@
-"C:\Program Files\Eggplant\Eggplant.bat" "D:\Vijayalaxmi\PracticeSuite.suite\Scripts\TestScript2.script"
+"C:\\Program Files\\Eggplant\\Eggplant.bat" "D:\\Vijayalaxmi\\PracticeSuite.suite\\Scripts\\TestScript2.script"
